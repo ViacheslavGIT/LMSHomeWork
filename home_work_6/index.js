f67eb2d46aecd5a880ecac7698dfa313a78895e2
ViacheslavGit <gigabazi@gmail.com>
2022-09-19 13:43:21 +0300

    fix code

--- home_work_6/index.js
+++ home_work_6/index.js
@@ -1,22 +1,24 @@
-const checkIfValueValid = (variable) => {
+const checkIfValueNotValid = (variable) => {
+  // undefined == null -> true
+  // undefined === null -> false
   if (variable == null || isNaN(variable)) {
-    return false;
+    return true;
   }
-  return true;
+  return false;
 };
 
 //1.Написати функцію, яка приймає 4 аргументи і шукає найменше число серед них
 
-const showMin = (num1, num2, num3, num4) => {
+const showMinNumber = (num1, num2, num3, num4) => {
   const numArray = [].concat(num1, num2, num3, num4);
-  const filteredArr = numArray.filter((el) => checkIfValueValid(el));
+  const filteredArr = numArray.filter((el) => !checkIfValueNotValid(el));
   if (filteredArr.length !== 0) {
     return `${Math.min(...filteredArr)} найменше число з вказаних`;
   } else {
     return "Введіть числа для обробки";
   }
 };
-console.log(showMin());
+console.log(showMinNumber());
 
 /*
 2.Написати функцію, яка приймає 2 аргументи та шукає найбільший спільний дільник для них.
@@ -26,9 +28,16 @@ console.log(showMin());
 const getGreatestCommonDivider = (num1, num2) => {
   const arrDividersNum1 = [];
 
-  if (!checkIfValueValid(num1) || !checkIfValueValid(num2)) {
-    return "Введіть два числа цифрами для обробки";
-  } else if (!Number.isInteger(+num1) || !Number.isInteger(+num2)) {
+  if (checkIfValueNotValid(num1) || checkIfValueNotValid(num2)) {
+    return "Введіть два числа для обробки";
+  }
+
+  // for typeof string and number
+  if (num1 == 0 || num2 == 0) {
+    return "Числа не повинні дорівнювати 0";
+  }
+
+  if (!Number.isInteger(+num1) || !Number.isInteger(+num2)) {
     return "Числа повинні бути цілі";
   } else {
     num1 = Math.abs(num1);
@@ -60,20 +69,20 @@ console.log(getGreatestCommonDivider());
 
 //3.Написати функцію, яка приймає 1 аргумент та перевіряє чи дане число ідеальне
 
-let perfectNum = 0;
+let perfectNumber = 0;
 const checkIfNumIsPerfect = (num) => {
   num = +num;
-  if (!checkIfValueValid(num)) {
+  if (checkIfValueNotValid(num)) {
     return "Введіть число";
   } else if (num < 2 || !Number.isInteger(num)) {
     return `${num} не ідеальне число`;
   } else {
     for (let i = 1; i < num; i++) {
       if (num % i === 0) {
-        perfectNum = perfectNum + i;
+        perfectNumber = perfectNumber + i;
       }
       if (i === num - 1) {
-        if (perfectNum === num) {
+        if (perfectNumber === num) {
           return `${num} ідеальне число!`;
         } else {
           return `${num} не ідеальне число`;
@@ -82,7 +91,7 @@ const checkIfNumIsPerfect = (num) => {
     }
   }
 };
-console.log(checkIfNumIsPerfect());
+console.log(checkIfNumIsPerfect(-4));
 
 /*
 4.Написати функцію, яка приймає 2 аргументи та обраховує суму в цьому проміжку. 
@@ -90,7 +99,7 @@ console.log(checkIfNumIsPerfect());
 Суму всіх чисел в цьому проміжку також треба обчислити
 */
 const getRange = (start, end) => {
-  if (!checkIfValueValid(start) || !checkIfValueValid(end)) {
+  if (checkIfValueNotValid(start) || checkIfValueNotValid(end)) {
     return "Будь ласка, введіть два числа";
   }
 
@@ -109,26 +118,27 @@ const getRange = (start, end) => {
   for (let i = +start; i <= +end; i++) {
     sum += i;
   }
-  return `Сума чисел в поміжку від ${start} до ${end} дорівнює ${sum}`;
+  return `Сума чисел в проміжку від ${start} до ${end} дорівнює ${sum}`;
 };
 console.log(getRange());
 
 //5.Написати функцію, яка приймає та конвертує температуру із Цельсій у Фаренгейт
 
-const convertCelsiusToFaringate = (celsius) => {
-  if (checkIfValueValid(celsius)) {
-    return `${celsius * 1.8 + 32}°F`;
-  } else {
+const convertCelsiusToFahrenheit = (celsius) => {
+  if (checkIfValueNotValid(celsius)) {
     return "Введіть градуси Цельсію (°C) цифрами";
+  } else {
+    return `${celsius * 1.8 + 32}°F`;
   }
 };
-console.log(convertCelsiusToFaringate());
+console.log(convertCelsiusToFahrenheit());
 
 //6.Написати фунцію, яка генерує випадкове ціле число в проміжку від 0 до 40
-
-const getRandomNum = () => {
+const MIN_NUMBER = 0;
+const MAX_NUMBER = 40;
+const getRandomNumber = () => {
   return `Рандомне число в проміжку від 0 до 40 = ${Math.floor(
-    Math.random() * 40
+    Math.random() * (MAX_NUMBER - MIN_NUMBER + 1) + MIN_NUMBER
   )}`;
 };
-console.log(getRandomNum());
+console.log(getRandomNumber());
