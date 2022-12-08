6ac227b240df6f6107e6783776b3a69b8d54d15b
ViacheslavGit <gigabazi@gmail.com>
2022-12-08 18:48:35 +0200

    initial commit

---
+++ my-burger-app/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import RenderPage from "./components/renderPage";
+
+function App() {
+  return (
+    <div className="App">
+      <RenderPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-burger-app/src/components/renderPage.jsx
@@ -0,0 +1,33 @@
+const RenderPage = () => {
+return <div id="container">
+        <header id="blog-name-container">
+            <div id="logo-wraper">
+                <span id="logo">ghghfg</span>
+            </div>
+            <div id='button-wrapper'>
+            <span className="btn">12</span>
+            <span className="btn">12</span>
+            <span className="btn">hfhfh</span>
+            <span className="btn">hfhfh</span>
+            <span className="btn">hfhfh</span>
+            </div>
+        </header>
+        <div id="banner">banner</div>
+        <div id="blog-news-wrapper">
+            <div id="blog">
+                <span>blog</span>
+                <span className="blog-massage">344</span>
+                <span className="blog-massage">33</span>
+                <span className="blog-massage">33</span>
+                <span className="blog-massage">33</span>
+            </div>
+            <div id="news">
+                <span>news</span>
+            <span className="news-massage">33</span>
+                <span className="news-massage">33</span>
+                <span className="news-massage">44</span>
+            </div>
+        </div>
+    </div>
+}
+export default RenderPage;
